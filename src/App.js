2d5b6113236225dc40197ed48f0bdc41b0a38f19
Aditya <sinha.aditya244@gmail.com>
2020-08-17 14:31:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import Settings from './components/Settings/Settings';
+import Profile from './components/Profile/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <h1> Finance Management App</h1>
+        </div>
+        <div>
+          <ul type="none">
+            <li><Link to="/home"> Home </Link></li>
+            <li><Link to="/settings"> Settings </Link></li>
+            <li><Link to="/profile"> Profile </Link></li>
+          </ul>
+        </div>
+        <Switch>
+          <Route path="/home" component={Home} exact/>
+          <Route path="/settings" component={Settings} exact/>
+          <Route path="/profile" component={Profile} exact/>
+        </Switch>
+      </BrowserRouter>
+    ); 
+  }
 }
 
 export default App;
